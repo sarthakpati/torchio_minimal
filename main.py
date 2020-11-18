a19d109f64c70d95c2aed3ae9fc397fec6dbd6ca
sarthakpati <sarthak.pati@hotmail.com>
2020-11-18 07:04:45 -0500

    initialize subjects dataset, sampler and queue

--- main.py
+++ main.py
@@ -27,3 +27,12 @@ transforms_list.append(Resample([1,1,1]))
 transforms_list.append(ZNormalization())
 
 transform = Compose(transforms_list)
+
+subjects_dataset = torchio.SubjectsDataset(subjects_list, transform=transform)
+
+sampler = torchio.data.UniformSampler([256,256,32])
+# all of these need to be read from model.yaml
+patches_queue = torchio.Queue(subjects_dataset, max_length=1,
+                              samples_per_volume=5,
+                              sampler=sampler, num_workers=0,
+                              shuffle_subjects=True, shuffle_patches=True, verbose=True)
