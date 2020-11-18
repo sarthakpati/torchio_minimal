a751ea1430eaa84ccf1e47b68074055da48e6126
sarthakpati <sarthak.pati@hotmail.com>
2020-11-18 10:14:39 -0500

    reduced

--- main.py
+++ main.py
@@ -11,21 +11,6 @@ from torchio.transforms import (ZNormalization,
 
 from torch.utils.data import DataLoader
 
-
-def one_hot(segmask_array, class_list):
-    batch_size = segmask_array.shape[0]
-    batch_stack = []
-    for b in range(batch_size):
-        one_hot_stack = []
-        segmask_array_iter = segmask_array[b,0]
-        for class_ in class_list:
-            bin_mask = (segmask_array_iter == int(class_))
-            one_hot_stack.append(bin_mask)
-        one_hot_stack = torch.stack(one_hot_stack)
-        batch_stack.append(one_hot_stack)
-    batch_stack = torch.stack(batch_stack)    
-    return batch_stack
-
 data_dir = './data' # put data in specific location
 
 subjects_list = []
